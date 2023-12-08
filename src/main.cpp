6201204a9c8176de5844bf920fb7c17fb427c0d8
Sam Der <sder@uci.edu>
2023-12-08 13:07:30 -0800

    fix: phase shift, pin usage

--- src/main.cpp
+++ src/main.cpp
@@ -4,38 +4,24 @@
 #include <cmath>
 #include <vector>
 
-const unsigned pinU1_H = 28;
-const unsigned pinU1_L = 27;
-const unsigned pinV1_H = 26;
-const unsigned pinV1_L = 22;
-
-const unsigned pinU2_H = 21;
-const unsigned pinU2_L = 20;
-const unsigned pinV2_H = 19;
-const unsigned pinV2_L = 18;
-
-const unsigned pinU3_H = 17;
-const unsigned pinU3_L = 16;
-const unsigned pinV3_H = 15;
-const unsigned pinV3_L = 14;
+const unsigned pinU_H = 21;
+const unsigned pinU_L = 20;
+const unsigned pinV_H = 19;
+const unsigned pinV_L = 18;
+const unsigned pinW_H = 17;
+const unsigned pinW_L = 16;
 
 int velocity = 0;
 
 void initialize_pins()
 {
 	const std::vector<unsigned> pins = {
-		pinU1_H,
-		pinU1_L,
-		pinV1_H,
-		pinV1_L,
-		pinU2_H,
-		pinU2_L,
-		pinV2_H,
-		pinV2_L,
-		pinU3_H,
-		pinU3_L,
-		pinV3_H,
-		pinV3_L};
+		pinU_H,
+		pinU_L,
+		pinV_H,
+		pinV_L,
+		pinW_H,
+		pinW_L};
 	for (unsigned pin : pins)
 	{
 		gpio_init(pin);
@@ -43,47 +29,59 @@ void initialize_pins()
 	}
 }
 
-void set_inverter_pins_(bool v, unsigned pinU_H, unsigned pinU_L, unsigned pinV_H, unsigned pinV_L)
+void set_inverter_pins_(bool v, unsigned pin_H, unsigned pin_L)
 {
-	gpio_put(pinU_H, v);
-	gpio_put(pinU_L, !v);
-	gpio_put(pinV_H, !v);
-	gpio_put(pinV_L, v);
+	gpio_put(pin_H, v);
+	gpio_put(pin_L, !v);
 }
 
 void run_one_inverter(int N)
 {
 	double qe = 0.0;
-	double qe2 = 0.0;
-	double qe3 = 0.0;
+	for (int i = 0; i < N; ++i)
+	{
+		double s = std::sin(2 * M_PI * i / N);
+		qe += s;
+		bool v = qe > 0;
+		int fix = v ? 1 : -1;
+		qe -= fix;
+		set_inverter_pins_(v, pinU_H, pinU_L);
+	}
+}
+
+void run_inverter(int N)
+{
+	double qeU = 0.0;
+	double qeV = 0.0;
+	double qeW = 0.0;
 
 	for (int i = 0; i < N; ++i)
 	{
-		double x = i * 2 * M_PI / N;
+		double x = 2 * M_PI * i / N;
 
-		double s = std::sin(x);			// 0 degrees
-		double s2 = std::sin(x + 5 * M_PI / 6); // 120 degrees
-		double s3 = std::sin(x + 4 * M_PI / 3); // 240 degrees
+		double sU = std::sin(x);				// 0 degrees
+		double sV = std::sin(x + 2 * M_PI / 3); // 120 degrees
+		double sW = std::sin(x + 4 * M_PI / 3); // 240 degrees
 
-		qe += s;
-		qe2 += s2;
-		qe3 += s3;
+		qeU += sU;
+		qeV += sV;
+		qeW += sW;
 
-		bool v = qe > 0;
-		bool v2 = qe2 > 0;
-		bool v3 = qe3 > 0;
+		bool vU = qeU > 0;
+		bool vV = qeV > 0;
+		bool vW = qeW > 0;
 
-		int fix = v ? 1 : -1;
-		int fix2 = v2 ? 1 : -1;
-		int fix3 = v3 ? 1 : -1;
+		int fixU = vU ? 1 : -1;
+		int fixV = vV ? 1 : -1;
+		int fixW = vW ? 1 : -1;
 
-		qe -= fix;
-		qe2 -= fix2;
-		qe3 -= fix3;
+		qeU -= fixU;
+		qeV -= fixV;
+		qeW -= fixW;
 
-		set_inverter_pins_(v, pinU1_H, pinU1_L, pinV1_H, pinV1_L);
-		set_inverter_pins_(v2, pinU2_H, pinU2_L, pinV2_H, pinV2_L);
-		set_inverter_pins_(v3, pinU3_H, pinU3_L, pinV3_H, pinV3_L);
+		set_inverter_pins_(vU, pinU_H, pinU_L);
+		set_inverter_pins_(vV, pinV_H, pinV_L);
+		set_inverter_pins_(vW, pinW_H, pinW_L);
 	}
 }
 
@@ -94,7 +92,7 @@ int main()
 
 	while (true)
 	{
-		run_one_inverter(frequency);
+		run_inverter(frequency);
 
 		if (time_us_32() % 10000000 > 5000000)
 		{
