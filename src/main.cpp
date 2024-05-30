4b5934c89834451a07aaa02e8679db35f0162bb3
Sam Der <sder@uci.edu>
2024-05-30 00:13:47 -0700

    Implement serial control of velocity and throttle (#39)
    * Implement serial control of velocity and throttle
    
    * Update naming/comments and refactor `run_inverter`
    
    * Use smaller mutex.h include
    
    ---------
    
    Co-authored-by: Taesung Hwang <taesungh@uci.edu>

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/mutex.h"
 #include "hardware/gpio.h"
 #include "tusb.h"
 
@@ -17,6 +19,12 @@ const float OPERATING_FREQUENCY = 45552.3;
 const float OFFSET = 0.0411;
 
 constexpr bool TEST_CIRCUIT = false;
+
+// Value will be changed by other core, so prevent compiler from optimizing as constant
+volatile static LimControlMessage lcm{ 0, 1 };
+// Allow only one core at a time to access lcm
+static mutex lcmMutex;
+
 
 void initialize_pins()
 {
@@ -103,6 +111,37 @@ int frequency_to_samples(float frequency)
 	return OPERATING_FREQUENCY / frequency - OFFSET;
 }
 
+// Secondary program to run on core 1
+void monitor_serial()
+{
+	while (true)
+	{
+		LimControlMessage message = read_control_message();
+
+		mutex_enter_blocking(&lcmMutex);
+		lcm.velocity = message.velocity;
+		lcm.throttle = message.throttle;
+		mutex_exit(&lcmMutex);
+	}
+}
+
+// Main program to run on core 0
+void run_inverter()
+{
+	while (true)
+	{
+		mutex_enter_blocking(&lcmMutex);
+		float frequency = calculate_frequency(lcm.velocity, lcm.throttle);
+		mutex_exit(&lcmMutex);
+
+		// TODO: disregard zero frequency
+
+		int N = frequency_to_samples(frequency);
+		// TODO: amplitude ratio
+		run_inverter_cycle(N, 1);
+	}
+}
+
 int main()
 {
 	stdio_init_all();
@@ -113,13 +152,11 @@ int main()
 
 	initialize_pins();
 
-	while (true)
-	{
-		LimControlMessage message = read_control_message();
-		float frequency = calculate_frequency(message.velocity, message.throttle);
-		int N = frequency_to_samples(frequency);
-		run_inverter_cycle(N, 1);
-	}
+	mutex_init(&lcmMutex);
+
+	// Run inverter on core 0 while updating control parameters on core 1
+	multicore_launch_core1(monitor_serial);
+	run_inverter();
 
 	return 0;
 }
